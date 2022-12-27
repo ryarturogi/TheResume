4105e4dd18261ad6b300514bbe1b1dd65211a14c
Ricardo Guillen I <ricardo.guillen@elaniin.com>
2022-12-27 13:31:43 -0400

    🚀 Initial commit

---
+++ pages/api/resume.js
@@ -0,0 +1,15 @@
+import fs from 'fs'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (_req, res) => {
+  const filePath = './public/resume.json'
+
+  fs.readFile(filePath, (error, data) => {
+    if (error) {
+      res.status(500).json({ message: 'Error reading file' })
+      return
+    }
+
+    res.status(200).json(JSON.parse(data))
+  })
+}
