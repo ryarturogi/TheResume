f51cb79d20cd88698e06809a9d5db10482fcc007
Ricardo Guillen I <ricardo.guillen@elaniin.com>
2022-12-27 14:29:56 -0400

    Fix lint errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
+
+App.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
